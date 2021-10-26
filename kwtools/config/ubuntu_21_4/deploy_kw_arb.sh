a59ed1449e9546fcc185b50c47c2ede9c5d0cfef
kerwin <kerwin19950830@gmail.com>
2021-10-26 13:05:14 +0800

    [update] deploy_kw_arb.sh

--- kwtools/config/ubuntu_21_4/deploy_kw_arb.sh
+++ kwtools/config/ubuntu_21_4/deploy_kw_arb.sh
@@ -4,7 +4,7 @@
 
 # 1. 下载'初始化ubunut'的shell脚本, 并执行
 wget "https://raw.githubusercontent.com/kerwin6182828/kwtools/main/kwtools/config/ubuntu_21_4/init_ubuntu.sh"
-# vim init_ubuntu.sh # 修改配置内容
+vim init_ubuntu.sh # 修改配置内容
 chmod +x init_ubuntu.sh
 source init_ubuntu.sh
 
@@ -22,11 +22,12 @@ source init_user.sh
 
 
 # 4. 上传二封的vnpy
-ip_address="202.182.110.161" # 需要更新ip
+remote_ip="18.183.104.223" # 需要更新ip
+remote_user="mirror"
 # 本地端操作 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 cd /usr/local/lib/python3.9/site-packages/vnpy-2.1.9-py3.9.egg
 tar zcvf vnpy.tar.gz vnpy # 压缩
-scp -r ./vnpy.tar.gz kerwin@${ip_address}:~/site-packages
+scp -r ./vnpy.tar.gz ${remote_user}@${remote_ip}:~/site-packages
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 cd ~/site-packages
 tar zxvf vnpy.tar.gz # 解压
@@ -38,9 +39,9 @@ pip install websocket-client --user pkg # vnpy的依赖库
 mkdir ~/kw_arb/kw_arb/secret
 # 本地端操作 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # 1. config.py
-scp -r ~/box/kw_arb/kw_arb/v2_5/config.py kerwin@${ip_address}:~/kw_arb/kw_arb/v2_5/config.py
+scp -r ~/box/kw_arb/kw_arb/v2_5/config.py ${remote_user}@${remote_ip}:~/kw_arb/kw_arb/v2_5/config.py
 # 2. secret.py
-scp -r ~/box/kw_arb/kw_arb/secret/secret.py kerwin@${ip_address}:~/kw_arb/kw_arb/secret/secret.py
+scp -r ~/box/kw_arb/kw_arb/secret/secret.py ${remote_user}@${remote_ip}:~/kw_arb/kw_arb/secret/secret.py
 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 # 6. 添加数据库索引
@@ -58,9 +59,9 @@ db.open_vspread_rate.getIndexes();
 
 # 7. 执行kw_arb项目
 # 方式一: 手动执行
-# nohup python3 -u /home/kerwin/kw_arb/kw_arb/v2_5/market.py > ~/log/market.log &
-# nohup python3 -u /home/kerwin/kw_arb/kw_arb/v2_5/calculate.py > ~/log/calculate.log &
-# nohup python3 -u /home/kerwin/kw_arb/kw_arb/v2_5/monitor.py > ~/log/monitor.log &
+# nohup python3 -u ~/kw_arb/kw_arb/v2_5/market.py > ~/log/market.log &
+# nohup python3 -u ~/kw_arb/kw_arb/v2_5/calculate.py > ~/log/calculate.log &
+# nohup python3 -u ~/kw_arb/kw_arb/v2_5/monitor.py > ~/log/monitor.log &
 # 方式二: shell脚本执行 (循环监控程序执行状态)
 cd
 wget "https://raw.githubusercontent.com/kerwin6182828/kwtools/main/kwtools/config/ubuntu_21_4/ensure_run.sh"
@@ -68,8 +69,10 @@ chmod +x ~/ensure_run.sh
 nohup ~/ensure_run.sh > ~/log/ensure_run.log & # 如果用nohup, 相当于就是用bash执行shell脚本了(不需要再加source了)
 
 # 查看项目运行的日志
-tail -32 ~/log/calculate.log
-tail -100 ~/log/monitor.log
+tail -f -n 100 ~/log/market.log # -f 循环读取
+tail -f -n 32 ~/log/calculate.log # -f 循环读取
+tail -f -n 100 ~/log/monitor.log
+tail -f -n 100 ~/log/ensure_run.log
 
 # 查看程序是否正在执行
 ps aux | grep ensure_run.sh
@@ -79,5 +82,9 @@ ps aux | grep monitor.py
 
 
 # 8. 卸载kw_arb项目, 重装
-rm -rf kw_arb
+rm -rf ~/kw_arb
 git clone -b kw-dev https://github.com/Adolf-L/kw_arb.git
+rm -rf ~/lcquant
+git clone -b kw-dev https://github.com/Adolf-L/lcquant.git
+pip uninstall kwtools
+pip install kwtools==
